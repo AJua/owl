cc652d5fa98aaab64f553fcdd97d3f7ad4f73149
AJua <ljredracess@gmail.com>
2019-04-20 15:29:54 +0800

    Add first model

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ActiveUser(models.Model):
+    count = models.DecimalField(max_digits=19, decimal_places=3)
+    time = models.DateTimeField()
+    def __str__(self):
+        return (f"{self.time}, {self.count}")
